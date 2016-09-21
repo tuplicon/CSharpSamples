8f22f46bedf520c53358b383c4e5151f9d2e39be
Chirag Patel <programmer.guru@gmail.com>
2016-09-21 18:23:01 -0400

    New solution for mongo db test

---
+++ NoSQL/MongoDb/MongoDb/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MongoDB.Bson;
+
+namespace MongoDb
+{
+    [Serializable]
+    public class Car
+    {
+        public ObjectId Id { get; set; } // requried by mongo
+        public string VehicleId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public Car()
+        {
+        }
+
+        public override string ToString()
+        {
+            return string.Format("VIN={0} Make={1} Model={2} Year={3}", VehicleId, Make, Model, Year);
+        }
+    }
+}
---
+++ NoSQL/MongoDb/MongoDb/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Driver;
+
+namespace MongoDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> carCollection = new List<Car>
+            {
+                new Car {Make = "TOYOTA", Model = "CAMRY", VehicleId = "VT1", Year = 2016},
+                new Car {Make = "TOYOTA", Model = "COROLA", VehicleId = "VT2", Year = 2016},
+                new Car {Make = "TOYOTA", Model = "AVELON", VehicleId = "VT3", Year = 2016},
+                new Car {Make = "HONDA", Model = "ACCORD", VehicleId = "VH1", Year = 2016},
+                new Car {Make = "HONDA", Model = "CIVIC", VehicleId = "VH2", Year = 2016},
+                new Car {Make = "HONDA", Model = "CROSSTOUR", VehicleId = "VH3", Year = 2016}
+           };
+
+            SampleDb.MyDb.CreateCollection("Cars");
+
+            var collection = SampleDb.MyDb.GetCollection<Car>("Cars");
+
+            collection.InsertMany(carCollection);
+
+            var selectedCollection = collection.AsQueryable().ToList();
+            foreach (var car in selectedCollection)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
