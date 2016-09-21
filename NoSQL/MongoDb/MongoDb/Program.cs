01ef03abe228d937e2816414cf09cec475cfdbe7
ParthSagar <sagar2parth@gmail.com>
2016-09-21 18:58:14 -0400

    added changes to main program

--- NoSQL/MongoDb/MongoDb/Program.cs
+++ NoSQL/MongoDb/MongoDb/Program.cs
@@ -8,21 +8,21 @@ namespace MongoDb
     {
         static void Main(string[] args)
         {
-            List<Car> carCollection = new List<Car>
-            {
-                new Car {Make = "TOYOTA", Model = "CAMRY", VehicleId = "VT1", Year = 2016},
-                new Car {Make = "TOYOTA", Model = "COROLA", VehicleId = "VT2", Year = 2016},
-                new Car {Make = "TOYOTA", Model = "AVELON", VehicleId = "VT3", Year = 2016},
-                new Car {Make = "HONDA", Model = "ACCORD", VehicleId = "VH1", Year = 2016},
-                new Car {Make = "HONDA", Model = "CIVIC", VehicleId = "VH2", Year = 2016},
-                new Car {Make = "HONDA", Model = "CROSSTOUR", VehicleId = "VH3", Year = 2016}
-           };
+           // List<Car> carCollection = new List<Car>
+           // {
+           //     new Car {Make = "TOYOTA", Model = "CAMRY", VehicleId = "VT1", Year = 2016},
+           //     new Car {Make = "TOYOTA", Model = "COROLA", VehicleId = "VT2", Year = 2016},
+           //     new Car {Make = "TOYOTA", Model = "AVELON", VehicleId = "VT3", Year = 2016},
+           //     new Car {Make = "HONDA", Model = "ACCORD", VehicleId = "VH1", Year = 2016},
+           //     new Car {Make = "HONDA", Model = "CIVIC", VehicleId = "VH2", Year = 2016},
+           //     new Car {Make = "HONDA", Model = "CROSSTOUR", VehicleId = "VH3", Year = 2016}
+           //};
 
-            SampleDb.MyDb.CreateCollection("Cars");
+           // SampleDb.MyDb.CreateCollection("Cars");
 
             var collection = SampleDb.MyDb.GetCollection<Car>("Cars");
 
-            collection.InsertMany(carCollection);
+            //collection.InsertMany(carCollection);
 
             var selectedCollection = collection.AsQueryable().ToList();
             foreach (var car in selectedCollection)
